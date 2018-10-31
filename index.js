46f859ae070ecac5b74aa4eb866dc7986c0f6075
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-31 01:07:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", e => {
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
 
-let listen = document.querySelector("div#hidden-div")
+let listen = document.querySelector('#hidden-div')
 
 listen.innerText('301')
 
